2ec22f281b83b45c9d17ef15b7f15c352edeb417
Mikevd6 <dagtakenlijst@gmail.com>
2026-06-08 10:22:00 +0200

    Add reusable exercise list component

---
+++ src/components/workouts/ExerciseList.jsx
@@ -0,0 +1,17 @@
+const ExerciseList = ({ title = 'Programma', kicker = 'Oefeningen', exercises }) => (
+  <section className="panel workout-exercises-panel">
+    <div className="panel-header">
+      <div>
+        <p className="panel-kicker">{kicker}</p>
+        <h3>{title}</h3>
+      </div>
+    </div>
+    <ul className="exercise-list">
+      {exercises.map((exercise) => (
+        <li key={exercise}>{exercise}</li>
+      ))}
+    </ul>
+  </section>
+);
+
+export default ExerciseList;
