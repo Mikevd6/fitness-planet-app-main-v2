695b0d3b1878ca18d11eb2297e1958c8ca1b6750
Mikevd6 <dagtakenlijst@gmail.com>
2026-06-08 13:03:59 +0200

    Allow Vite to build existing JSX in js files

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,23 @@
-import { defineConfig } from 'vite';
+import { defineConfig, transformWithEsbuild } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    {
+      name: 'load-js-files-as-jsx',
+      async transform(code, id) {
+        if (!/src[\\/].*\.js$/.test(id)) {
+          return null;
+        }
+
+        return transformWithEsbuild(code, id, {
+          loader: 'jsx',
+          jsx: 'automatic'
+        });
+      }
+    },
+    react({
+      include: /\.[jt]sx?$/
+    })
+  ]
 });
