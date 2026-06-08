61877cdabe7107d5569ca6ce0f1a87e0bb9eb353
Mikevd6 <dagtakenlijst@gmail.com>
2026-06-08 09:26:45 +0200

    Add workout data for dynamic routes

---
+++ src/data/workouts.js
@@ -0,0 +1,50 @@
+export const workoutSessions = [
+  {
+    id: 'back-shoulders',
+    title: 'Back & Shoulders',
+    time: 'Afgelopen weekend',
+    type: 'Functioneel',
+    duration: '58 minuten',
+    intensity: 'Middel',
+    calories: 420,
+    description: 'Een functionele training gericht op rug, schouders en houding. Ideaal om kracht en stabiliteit op te bouwen.',
+    exercises: [
+      'Lat pulldown - 3 sets van 10 herhalingen',
+      'Shoulder press - 3 sets van 8 herhalingen',
+      'Seated row - 3 sets van 12 herhalingen',
+      'Face pulls - 3 sets van 15 herhalingen'
+    ]
+  },
+  {
+    id: 'hit-cardio',
+    title: 'HIT Cardio',
+    time: '11 Januari 2021',
+    type: 'Cardio',
+    duration: '32 minuten',
+    intensity: 'Hoog',
+    calories: 510,
+    description: 'Een korte maar intensieve cardio workout met intervallen. Gericht op conditie, tempo en vetverbranding.',
+    exercises: [
+      'Sprint interval - 8 rondes van 30 seconden',
+      'Mountain climbers - 4 sets van 40 seconden',
+      'Burpees - 4 sets van 12 herhalingen',
+      'Rustige cooldown - 6 minuten'
+    ]
+  },
+  {
+    id: 'bootcamp',
+    title: 'Bootcamp',
+    time: 'Maandag',
+    type: 'Volledig lichaam',
+    duration: '45 minuten',
+    intensity: 'Hoog',
+    calories: 475,
+    description: 'Een full-body sessie met kracht, conditie en core-oefeningen. Geschikt als stevige weekstart.',
+    exercises: [
+      'Squats - 4 sets van 12 herhalingen',
+      'Push-ups - 4 sets van 10 herhalingen',
+      'Walking lunges - 3 sets van 20 stappen',
+      'Plank - 3 rondes van 45 seconden'
+    ]
+  }
+];
