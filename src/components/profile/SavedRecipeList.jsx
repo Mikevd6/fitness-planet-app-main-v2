426652c5031cf1ec60e580bc26988f3a4992e508
Mikevd6 <dagtakenlijst@gmail.com>
2026-06-08 10:22:21 +0200

    Add reusable saved recipe list

---
+++ src/components/profile/SavedRecipeList.jsx
@@ -0,0 +1,23 @@
+import EmptyState from '../ui/EmptyState';
+import SavedRecipeCard from './SavedRecipeCard';
+
+const SavedRecipeList = ({ recipes, onRemoveRecipe }) => {
+  if (recipes.length === 0) {
+    return (
+      <EmptyState
+        className="profile-settings__empty"
+        message="Je hebt nog geen recepten opgeslagen. Ga naar Recepten om een favoriet te bewaren."
+      />
+    );
+  }
+
+  return (
+    <div className="profile-settings__recipes">
+      {recipes.map((recipe) => (
+        <SavedRecipeCard key={recipe.id} recipe={recipe} onRemove={onRemoveRecipe} />
+      ))}
+    </div>
+  );
+};
+
+export default SavedRecipeList;
